1f744c75e16443b65011a904d8d2ce14f2d87d85
xiaolvershurui <15120098921@163.com>
2018-05-25 09:29:36 +0800

    4

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import  React from 'react';
+import  ReactDom  from 'react-dom';
+
+
+
+
+class A extends React.Component{
+    render(){
+
+    }
+
+}
+
+
+ReactDom.render(<A/>,documnet.querySelector('.app'))
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let webpack = require('webpack');
+module.exports = {
+
+    entry: "./src/index.js",
+    devtool:'source-map',
+    output: {
+    filename: 'build.js',
+        // 必须是一个绝对路径
+        path: require('path').resolve('dist')
+    },
+    // 解析
+    resolve: {
+        alias: { // 必须是绝对路径
+            'src': require('path').resolve('src')
+        }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index1.html',
+        })
+    ],
+    devServer: {
+        port: 8003,
+        // 实现支持Broswer路由
+        historyApiFallback: true,
+        hot:true
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            { test: /.(jpg|png|gif)$/, use: 'url-loader?limit=8000' }
+        ]
+    }
+}
